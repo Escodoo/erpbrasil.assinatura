30615312e5cd8879bff93d0a227666c2a1895946
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-05-17 14:54:06 -0300

    [HACK] workaround nfselib paulistana

--- src/erpbrasil/assinatura/certificado.py
+++ src/erpbrasil/assinatura/certificado.py
@@ -6,97 +6,104 @@ import tempfile
 import datetime
 
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization.pkcs12 import (
-    load_key_and_certificates,
-)
-from cryptography.hazmat.primitives import serialization
-from cryptography.x509.oid import NameOID
-
-import pytz
+from cryptography.hazmat.primitives.serialization.pkcs12 import load_key_and_certificates
+from OpenSSL import crypto
+from pytz import UTC
 
 from .excecoes import CertificadoExpirado
 from .excecoes import CertificadoSenhaInvalida
 from .excecoes import ErroDeLeituraDeArquivo
 
 
-class Certificado():
-    """Classe para representar o certificado digital"""
+class Certificado(object):
 
     def __init__(self, arquivo, senha, raise_expirado=True):
         """Permite informar um arquivo PFX binario ou o path do arquivo"""
 
-        self._senha = self._encode_senha(senha)
-
-        if os.path.exists(arquivo):
-            try:
-                self._arquivo = open(arquivo, "rb").read()
-            except IOError as io_error:
-                raise ErroDeLeituraDeArquivo("Erro ao ler o arquivo!!!") from io_error
-        elif isinstance(arquivo, bytes):
-            self._arquivo = base64.b64decode(arquivo)
-        elif isinstance(arquivo, str):
-            self._arquivo = arquivo
+        self._senha = senha
 
         try:
-            self.key, self.cert, self.othercerts = self._load_key_and_certificates()
-        except ValueError as value_error:
-            raise CertificadoSenhaInvalida(
-                "Certificado ou senha inválida!!!"
-            ) from value_error
+            try:
+                self._arquivo = base64.b64decode(arquivo)
 
-        if raise_expirado and self.expirado:
-            raise CertificadoExpirado("Certificado Expirado!!!")
+                # Salva o arquivo pfx no formato binario pkc12
+                self._pkcs12 = crypto.load_pkcs12(self._arquivo,
+                                                  self._senha)
+            except Exception:
+                if isinstance(arquivo, bytes):
+                    self._arquivo = arquivo
+                elif isinstance(arquivo, str):
+                    self._arquivo = open(arquivo, 'rb').read()
 
-        self._chave = self.key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-        self._cert = self.cert.public_bytes(encoding=serialization.Encoding.PEM)
+                # Salva o arquivo pfx no formato binario pkc12
+                self._pkcs12 = crypto.load_pkcs12(self._arquivo,
+                                                  self._senha)
+
+            # Extrai o certicicado
+            self._cert = crypto.dump_certificate(crypto.FILETYPE_PEM,
+                                                 self._pkcs12.get_certificate())
+
+            # Extrai a chave
+            self._chave = crypto.dump_privatekey(crypto.FILETYPE_PEM,
+                                                 self._pkcs12.get_privatekey())
+
+            self._x509 = crypto.load_certificate(crypto.FILETYPE_PEM,
+                                                 self._cert)
+
+            self.key, self.cert, self.othercerts = \
+                self._load_key_and_certificates()
+
+        except IOError:
+            raise ErroDeLeituraDeArquivo('Erro ao ler o arquivo!!!')
+        except crypto.Error:
+            raise CertificadoSenhaInvalida('Certificado ou senha inválida!!!')
+
+        if raise_expirado and self._x509.has_expired():
+            raise CertificadoExpirado('Certificado Expirado!!!')
 
     def _load_key_and_certificates(self):
         """
         :return:
         """
         return load_key_and_certificates(
-            data=self._arquivo, password=self._senha, backend=default_backend()
+            data=self._arquivo,
+            password=self._senha.encode(),
+            backend=default_backend()
         )
 
     @property
     def inicio_validade(self):
         """Pega a data inicial de validade do certificado"""
-        return self.cert.not_valid_before.replace(tzinfo=pytz.UTC)
+        return UTC.localize(self.cert.not_valid_before)
 
     @property
     def fim_validade(self):
         """Pega a data final de validade do certificado"""
-        return self.cert.not_valid_after.replace(tzinfo=pytz.UTC)
+        return UTC.localize(self.cert.not_valid_after)
 
     @property
     def emissor(self):
         """Pega o nome do emissor do certificado"""
-        return self.cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        return self._x509.get_issuer().CN
 
     @property
     def proprietario(self):
         """Pega o nome do proprietário do certificado"""
-        return self.cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        return self._x509.get_subject().CN
 
     @property
     def cnpj_cpf(self):
         """Pega o CNPJ ou CPF do proprietário do certificado"""
         # As vezes tem o nome e cnpj_cpf do proprietário
         proprietario = self.proprietario
-        if ":" in proprietario:
-            cnpj_cpf = proprietario.rsplit(":", 1)[1]
+        if ':' in proprietario:
+            cnpj_cpf = proprietario.rsplit(':', 1)[1]
             return cnpj_cpf
-        return ""
+        return ''
 
     @property
     def expirado(self):
-        """Verifica se o certificado está expirado"""
-        today = datetime.datetime.today()
-        if today > self.cert.not_valid_after:
+        if self._x509.has_expired():
             return True
         return False
 
@@ -104,16 +111,13 @@ class Certificado():
         """Retorna o certificado e a chave"""
         return self._cert.decode(), self._chave.decode()
 
-
-    @staticmethod
-    def _encode_senha(senha):
-        if isinstance(senha, str):
-            return senha.encode()
-        return senha
+    def pkcs12(self):
+        """Retorna o arquivo pfx no formato binario pkc12"""
+        return self._pkcs12
 
 
-class ArquivoCertificado():
-    """Classe para ser utilizada quando for necessário salvar o arquivo
+class ArquivoCertificado(object):
+    """ Classe para ser utilizada quando for necessário salvar o arquivo
     temporariamente, garantindo a segurança que o mesmo sera salvo e apagado
     rapidamente
 
@@ -130,10 +134,10 @@ class ArquivoCertificado():
 
         cert, key = certificado.cert_chave()
 
-        tmp = os.fdopen(self.key_fd, "w")
+        tmp = os.fdopen(self.key_fd, 'w')
         tmp.write(cert)
 
-        tmp = os.fdopen(self.cert_fd, "w")
+        tmp = os.fdopen(self.cert_fd, 'w')
         tmp.write(key)
 
     def __enter__(self):
